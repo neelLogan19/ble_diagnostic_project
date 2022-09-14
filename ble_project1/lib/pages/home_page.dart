8eb27e83421f6d9e7cd3e7ad0540f4234ca3bc65
Neelkanth Vashist <neelkanthk@napino.com>
2022-09-14 17:54:00 +0530

    data recieved

--- ble_project1/lib/pages/home_page.dart
+++ ble_project1/lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'dart:async'; // support for async programming with classes such as future and stream
@@ -19,11 +22,19 @@ class _HomePageState extends State<HomePage> {
 
   //scanning variable is set to true when scanning starts
   bool _scanStarted = false;
-  late DiscoveredDevice _napinoDevice;
+  //if we are connected to any device make it true
+  bool _connected = false;
+  //stream for scanning bluetooth devices
   late StreamSubscription<DiscoveredDevice> _scanStream;
-
+  //storing bluetooth devices
   List<DiscoveredDevice> devicesList = [];
-  String _deviceName = "";
+  //this hashset is used to filter out dupliate devices
+  final hs = HashSet<String>();
+
+  var decoded;
+
+  late QualifiedCharacteristic _rxCharacteristic;
+
   // if there is no device available after scanning we print this message
   String _deviceMsg = "";
   //--> when are scanning is completed and we have all the devie in the list this variable is set to true
@@ -32,7 +43,7 @@ class _HomePageState extends State<HomePage> {
   //serviceUid and characteristic uuid
   final Uuid serviceUuid = Uuid.parse("edd79b0f-4ae0-484a-8ba9-4a611f1cb47b");
   final Uuid characteristicUuid =
-      Uuid.parse("6ACF4F08-CC9D-D495-6B41-AA7E60C4E8A6");
+      Uuid.parse("d775dbdb-3b38-47be-9734-9ff022ba37ea");
 
   //this function is called on button click so that scanning for bluetooth device starts.
   void _startScan() async {
@@ -50,26 +61,82 @@ class _HomePageState extends State<HomePage> {
     }
 // Main scanning logic happens here -->here we add all devices into a list so that we can display it late
     if (permGranted) {
-      _scanStream =
-          flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
+      _scanStream = flutterReactiveBle
+          .scanForDevices(withServices: [serviceUuid]).listen((device) {
         //code for handling results
-        // String res = device.name.toString();
-        print(device);
-        // setState(() {
-        //   _napinoDevice = device;
-        //   _foundDeviceWaitingToConnect = true;
-        // });
+        String res = device.name.toString();
+        if (!hs.contains(res)) {
+          devicesList.add(device);
+          hs.add(res);
+        }
+
+        // print(hm);
+        // print(device);
+        setState(() {
+          _foundDeviceWaitingToConnect = true;
+        });
       }, onError: (ErrorDescription) {
         //code for handling error
         // print(ErrorDescription);
       });
 
-      Timer(Duration(seconds: 5), () {
+      Timer(Duration(seconds: 2), () {
         _scanStream.cancel();
       });
     }
+  }
 
-    // print(_napinoDevice);
+  void performTask(dev) async {
+    final characteristic = QualifiedCharacteristic(
+        serviceId: serviceUuid,
+        characteristicId: characteristicUuid,
+        deviceId: dev);
+    final response =
+        await flutterReactiveBle.readCharacteristic(characteristic);
+    decoded = utf8.decode(response);
+    print(response);
+    print(decoded);
+  }
+
+  void _connectToDevice(deviceIde) {
+    // We're done scanning, we can cancel it
+    _scanStream.cancel();
+    // Let's listen to our connection so we can make updates on a state change
+    Stream<ConnectionStateUpdate> _currentConnectionStream = flutterReactiveBle
+        .connectToAdvertisingDevice(
+            id: deviceIde,
+            prescanDuration: const Duration(seconds: 1),
+            withServices: [serviceUuid, characteristicUuid]);
+    _currentConnectionStream.listen((event) {
+      switch (event.connectionState) {
+        // We're connected and good to go!
+        case DeviceConnectionState.connected:
+          {
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: serviceUuid,
+                characteristicId: characteristicUuid,
+                deviceId: event.deviceId);
+            print("good to go");
+            final characteristic = QualifiedCharacteristic(
+                serviceId: serviceUuid,
+                characteristicId: characteristicUuid,
+                deviceId: deviceIde);
+            performTask(deviceIde);
+            setState(() {
+              // _foundDeviceWaitingToConnect = false;
+              _connected = true;
+            });
+            break;
+          }
+        // Can add various state state updates on disconnect
+        case DeviceConnectionState.disconnected:
+          {
+            print("not good to go");
+            break;
+          }
+        default:
+      }
+    });
   }
 
   @override
@@ -84,7 +151,27 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: ListView(),
+      body: Container(
+        child: (_foundDeviceWaitingToConnect && devicesList.length > 0)
+            ? ListView.builder(
+                itemCount: devicesList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(devicesList[index].name),
+                      subtitle: Text(devicesList[index].id),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        _connectToDevice(devicesList[index].id);
+                      },
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: Text("no device found"),
+              ),
+      ),
       floatingActionButton: Wrap(
         direction: Axis.horizontal,
         children: [
@@ -105,14 +192,6 @@ class _HomePageState extends State<HomePage> {
                     onPressed: _startScan,
                   ),
                 ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: FloatingActionButton(
-              child: Icon(Icons.bluetooth),
-              onPressed: () {},
-              backgroundColor: Colors.grey,
-            ),
-          ),
         ],
       ),
     );
