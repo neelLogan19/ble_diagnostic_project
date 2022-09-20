86dcaa2da389fe3bb5ecf862bec817f5bc3fb653
Neelkanth Vashist <neelkanthk@napino.com>
2022-09-20 15:24:20 +0530

    data fetched and displayed

--- ble_project1/lib/pages/data_page.dart
+++ ble_project1/lib/pages/data_page.dart
@@ -18,7 +18,8 @@ class DataPage extends StatefulWidget {
 
 class _DataPageState extends State<DataPage> {
   var decoded;
-
+  var resp;
+  List<String> res = [];
   final Uuid serviceUuid = Uuid.parse("edd79b0f-4ae0-484a-8ba9-4a611f1cb47b");
 
   final Uuid characteristicUuid =
@@ -34,19 +35,69 @@ class _DataPageState extends State<DataPage> {
     final response =
         await flutterReactiveBle.readCharacteristic(characteristic);
     decoded = utf8.decode(response);
-    print(response);
+    // print(response);
     print(decoded.toString());
+    String ans = decoded.toString();
+    print(ans);
+    res.add(ans);
+    print(res);
+    setState(() {
+      resp = decoded.toString();
+      // res.add(ans);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${widget.name} Details'),
+        automaticallyImplyLeading: false,
+        title: Text('${widget.name}'),
+        actions: <Widget>[
+          FlatButton(
+            textColor: Colors.white,
+            onPressed: () {},
+            child: Text("Disconnect"),
+            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Your device id is ${widget.id}'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 3,
+            ),
+            Text(
+              widget.id,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Card(
+                elevation: 5,
+                child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 510,
+                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      child: Text(
+                          res.toString().length <= 10 ? "" : res.toString(),
+                          style: const TextStyle(color: Colors.black)),
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
+      // Center(
+      //   child: Text('Your device id is ${widget.id} and ${resp}'
+      //   ),
+
+      // ),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
         child: Container(height: 50.0),
